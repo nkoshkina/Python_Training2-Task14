9709a4c555a15367ee3fd847eb9f2c8e60f6c59d
nkoshkina <nkoshkina@gmail.com>
2022-01-26 11:41:50 +0300

    commit for Task 14

--- test/test_home_page_data.py
+++ test/test_home_page_data.py
@@ -18,6 +18,7 @@ def test_data_on_home_and_edit_pages(app):
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_home_page(contact_from_edit_page)
     assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_home_page(contact_from_edit_page)
 
+
 def clear_phones(s):
     return re.sub("()/ -", "", s)
 
